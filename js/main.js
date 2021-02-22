603e59790d5ac3c7f41139c30b7215434c9fefec
daisuke sasaki <gerund25495641@gmail.com>
2021-02-23 00:39:01 +0900

    正解した文字を埋め込みすることに成功

--- js/main.js
+++ js/main.js
@@ -5,9 +5,30 @@ for (let i = 0; i < 5; i++){
 }
 
 {
+    const word = 'red';
+    let loc = 0;
+
+
+    const target = document.getElementById('target');
+    target.textContent = word;
+    
+
+
+
+
+
     document.addEventListener('keydown', e =>{
-        const target = document.getElementById('target');
-        target.textContent = e.key;
+        if (e.key === word[loc]){
+            loc++;
+
+
+
+
+            // 1: _ed
+            // 2: __d
+            // 3: ___
+            target.textContent = '_'.repeat(loc) + word.substring(loc);
+        }
     });
 }
 
