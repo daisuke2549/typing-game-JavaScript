82fcb08108a866dc36c8ba44b2a97ae1d1645f8b
daisuke sasaki <gerund25495641@gmail.com>
2021-02-23 07:39:57 +0900

    単語を入 力後、次の単語に遷移するように実装済み

--- js/main.js
+++ js/main.js
@@ -3,31 +3,49 @@
 for (let i = 0; i < 5; i++){
     console.log("現在の数字は" + i +  "です");
 }
+console.log(100+200);
+
+
+
 
 {
-    const word = 'red';
+   function setWord(){
+    word = words[Math.floor(Math.random() * words.length)];
+    target.textContent = word;
+    loc = 0;
+   }
+
+
+　　const words = [
+       'red',
+       'blue',
+       'pink',
+   ];
+
+    let word;
     let loc = 0;
 
 
     const target = document.getElementById('target');
-    target.textContent = word;
-    
 
-
+    setWord();
 
 
 
     document.addEventListener('keydown', e =>{
-        if (e.key === word[loc]){
+        if (e.key !== word[loc]){
+            return;
+        }
+
             loc++;
 
+        // 1: _ed
+        // 2: __d
+        // 3: ___
+        target.textContent = '_'.repeat(loc) + word.substring(loc);
 
-
-
-            // 1: _ed
-            // 2: __d
-            // 3: ___
-            target.textContent = '_'.repeat(loc) + word.substring(loc);
+        if(loc === word.length){
+            setWord();
         }
     });
 }
