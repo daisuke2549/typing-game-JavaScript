b4653a352f9fc0cfaa6ec91f4331ba1a1a301c15
daisuke sasaki <gerund25495641@gmail.com>
2021-02-23 00:22:34 +0900

    打ち込んだキーを表示することに成功

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+'use strict';
+
 for (let i = 0; i < 5; i++){
     console.log("現在の数字は" + i +  "です");
 }
+
+{
+    document.addEventListener('keydown', e =>{
+        const target = document.getElementById('target');
+        target.textContent = e.key;
+    });
+}
+
+
