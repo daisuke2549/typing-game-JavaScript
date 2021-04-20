b6f38e6eabed10514e404f081be6532d877ffceb
Daisuke Sasaki <gerund25495641@gmail.com>
2021-04-20 23:25:03 +0900

    問題数を切り替えた表示の実装まで完了

--- js/main.js
+++ js/main.js
@@ -3,28 +3,42 @@ $(function() {
  const $yomi = $('#yomi');
  const $mondai = $('#mondai');
  const $finishPanel = $('#finish-panel');
+ const $countSelect = $('#count-select');
  
  // 問題用の変数の初期化
  let char_index = 1;
  let max_length = 3; // TODO 最初の問題
- let question_number = 0;
+ let question_number = 1;
  let question_limit = 3;
+ 
  
  // 問題
  const MONDAI_LIST = [
-   {yomi:'ごはん', text:'gohan'},
-   {yomi:'おすし', text:'osushi'},
-   {yomi:'サイフ', text:'saifu'},
-   {yomi:'バナナ', text:'banana'},
-   {yomi:'くつした', text:'kutsushita'},
- ];
+  {yomi:'ごはん', text:'gohan'},
+  {yomi:'おすし', text:'osushi'},
+  {yomi:'サイフ', text:'saifu'},
+  {yomi:'バナナ', text:'banana'},
+  {yomi:'くつした', text:'kutsushita'},
+  {yomi:'なべ', text:'nabe'},
+  {yomi:'あし', text:'ashi'},
+  {yomi:'パソコン', text:'pasokon'},
+  {yomi:'けいたい', text:'keitai'},
+  {yomi:'ふとん', text:'futon'},
+];
  
+changeQuestionWord(0);
+
+$countSelect.on('change', function(e){
+  question_limit = Number($countSelect.val());
+  changeQuestionWord(0);
+});
+
  $(document).on('keypress', function(e){
    // console.log('key:'+e.key);
    const $target = $('#char-'+char_index);
    const char = $target.text();
    if (e.key === char) { //入力文字と現在の位置の文字が一緒だったら
-     // alert('正解!');
+      // alert('正解!');
      $target.removeClass('default');
      $target.addClass('correct');
      char_index++;
